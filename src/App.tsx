2f3688abadc04e194cf44c7dd7d7e35c34ac288b
Marcin Rudzki <mar.rudzki@gmail.com>
2022-10-02 08:28:08 +0200

    set initial color

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState('');
+
+  useEffect(() => {
+    // TODO: generate a random color here
+    setColor('#FF00DD')
+  }, []);
+
   return (
     <div className="App">
-      <div className="guess-me"></div>
+      <div className="guess-me" style={{ background: color }}></div>
     </div>
   );
 }
