ead24c7eedbb6644ce68c3aedcbfce5401b81880
Marcin Rudzki <mar.rudzki@gmail.com>
2022-10-02 08:43:42 +0200

    generate random colors

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,14 @@ import './App.css';
 function App() {
   const [color, setColor] = useState('');
 
+  const getRandomColor = () => {
+    const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+    const color = new Array(6).fill('').map(() => digits[Math.floor(Math.random() * digits.length)]).join('');
+    return `#${color}`;
+  }
+
   useEffect(() => {
-    // TODO: generate a random color here
-    setColor('#FF00DD')
+    setColor(getRandomColor());
   }, []);
 
   return (
