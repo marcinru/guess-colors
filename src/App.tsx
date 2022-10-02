7718f5fcebff03995d03491d9c6f091959f682f1
Marcin Rudzki <mar.rudzki@gmail.com>
2022-10-02 08:53:55 +0200

    add random answers

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   const [color, setColor] = useState('');
+  const [answers, setAnswers] = useState<string[]>([]);
 
   const getRandomColor = () => {
     const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
@@ -11,12 +12,19 @@ function App() {
   }
 
   useEffect(() => {
-    setColor(getRandomColor());
+    const actualColor = getRandomColor();
+    setColor(actualColor);
+    setAnswers([actualColor, getRandomColor(), getRandomColor()].sort(() => 0.5 - Math.random()));
   }, []);
 
   return (
     <div className="App">
-      <div className="guess-me" style={{ background: color }}></div>
+      <div>
+        <div className="guess-me" style={{ background: color }}></div>
+        {answers.map(answer => (
+          <button key={answer}>{answer}</button>
+        ))}
+      </div>
     </div>
   );
 }
