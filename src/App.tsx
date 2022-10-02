66a5de2b7d04d08c806bbf7cef47a16fbf57731d
Marcin Rudzki <mar.rudzki@gmail.com>
2022-10-02 09:22:19 +0200

    regenerate colors after correct answer

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
+const getRandomColor = () => {
+  const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  const color = new Array(6).fill('').map(() => digits[Math.floor(Math.random() * digits.length)]).join('');
+  return `#${color}`;
+}
+
 function App() {
   const [color, setColor] = useState('');
   const [answers, setAnswers] = useState<string[]>([]);
   const [isWrongSelection, setIsWrongSelection] = useState<boolean>(false);
 
-  const getRandomColor = () => {
-    const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-    const color = new Array(6).fill('').map(() => digits[Math.floor(Math.random() * digits.length)]).join('');
-    return `#${color}`;
-  }
-
-  useEffect(() => {
+  const generateColors = () => {
+    getRandomColor();
     const actualColor = getRandomColor();
     setColor(actualColor);
     setAnswers([actualColor, getRandomColor(), getRandomColor()].sort(() => 0.5 - Math.random()));
+  }
+
+  useEffect(() => {
+    generateColors();
   }, []);
 
-  function handleAnswerClicked(answer: string) {
+  const handleAnswerClicked = (answer: string) => {
     if (answer === color) {
       setIsWrongSelection(false);
-      // TODO: reselect colors
+      generateColors();
     } else {
       setIsWrongSelection(true);
     }
